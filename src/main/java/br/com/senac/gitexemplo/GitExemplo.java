aef83ae10da93729b493499404b0381b3fb8afbd
Alexsandro Bortoli Machado <alexsandromachado884@gmail.com>
2023-08-30 22:51:53 -0300

    hello World

--- src/main/java/br/com/senac/gitexemplo/GitExemplo.java
+++ src/main/java/br/com/senac/gitexemplo/GitExemplo.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package br.com.senac.gitexemplo;
 
-/**
- *
- * @author Usuário
- */
+
 public class GitExemplo {
 
     public static void main(String[] args) {
